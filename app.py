fad49da521a7993bf30da2a7837dc4ed1d605be4
yehudise <yehudise02@gmail.com>
2024-05-16 14:46:48 -0500

    is this working?

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+responses = []
+
+@app.route('/')
+def index():
+    title = "Customer Satisfaction Survey"
+    instructions = "Please fill out a survey about your experience with us."
+    return render_template('index.html', title=title, instructions=instructions)
 
