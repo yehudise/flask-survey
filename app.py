589096a62c65b5ebe8b7ce39a3df8dae936d72fc
yehudise <yehudise02@gmail.com>
2024-05-19 04:44:00 -0500

    I think I'm getting somewhere

--- app.py
+++ app.py
@@ -1,39 +1,66 @@
-from flask import Flask, render_template,request,redirect, url_for
+from flask import Flask, render_template,request,redirect,flash,session
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import Question, Survey
+from surveys import satisfaction_survey as survey
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "never-tell!"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-responses = []
+responses_key = "responses"
+debug = DebugToolbarExtension(app)
 
-from surveys import satisfaction_survey
+@app.route("/")
+def show_survey_start():
+    """Choose a survey."""
+    return render_template("start_survey.html", survey=survey)
 
-@app.route('/')
-def index():
-    title = "Customer Satisfaction Survey"
-    instructions = "Please fill out a survey about your experience with us."
-    return render_template('index.html', title=title, instructions=instructions)
+@app.route("/begin", methods=["POST"])
+def start_survey():
+    """Clear all the responses from the session"""
+    session[responses_key]= []
 
-@app.route('/questions/<int:question_id>', methods=['GET', 'POST'])
-def question(question_id):
-    if request.method == 'POST':
-        answer = request.form.get('answer')
-        responses.append(answer)
-        next_question_id = question_id + 1
-        if next_question_id < len(satisfaction_survey.questions):
-            return redirect(url_for('question', question_id=next_question_id))
-        else:
-            return redirect(url_for('thank_you'))
+    return redirect("/questions/0")
+
+@app.route("/answer", methods=["POST"])
+def handle_question():
+    """Save the response from the taker and move them to the next question"""
+
+    #this line of code gets the answer that the person put in 
+    choice=request.form['answer']
+
+    #this line takes responses and connects it to this users "sesion" so they could store the users answers
+    responses = session[responses_key]
+    #this line of code takes the users choice and adds it to respnses
+    responses.append(choice)
+    #this line of code updates responses of this users session
+    session[responses_key]= responses
+
+    #this if statement checks to see if after this anser that the use inputs if there are more question to the surevy or if he completed it redirects him to complete page
+    if (len(responses) == len(survey.questions)):
+        return redirect("/complete")
     else:
-        if question_id < len(satisfaction_survey.questions):
-            current_question = satisfaction_survey.questions[question_id]
-            question_text = current_question.question
-            choices = current_question.choices
-            return render_template('question.html', question_text=question_text, choices=choices)
-        else:
-            return "Invalid question ID"
+        return redirect(f"/questions/{len(responses)}")
+    
+@app.route("/questions/<int:qid>")
+def show_question(qid):
+    responses = session.get(responses_key)
 
-@app.route('/thank_you')
-def thank_you():
-    return "Thank you for completing the survey!"
+    if (responses is None):
+        return redirect("/")
 
+    # this if statement will redirect the user to the complete page of the number of responses he gave equals the number of question (i.e. - he completed the surevy)
+    if (len(responses) == len(survey.questions)):
+        return redirect("/complete")
 
+    if (len(responses) != qid):
+        flash(f"Invalid question id: {qid}.")
+        return redirect(f"/questions/{len(responses)}")
+    
+    question= survey.questions[qid]
+    return render_template("question.html", question_num=qid, question=question)
+
+@app.route("/cpmplete")
+def complete():
+    """this is the survey complete route - the page that will show when surevy is complete!"""
+
+    return render_template("completion.html")
